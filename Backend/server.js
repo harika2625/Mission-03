b5bc7e268e854c4723a776ec57b226472b01665f
Harika Vonguri <harikavonguri@gmail.com>
2025-03-17 15:32:13 +1300

    Initial setup done

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
