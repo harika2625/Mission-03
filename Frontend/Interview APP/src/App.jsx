4ba6ff5627700077a31add78b16c1a7f37cb61ff
Harika Vonguri <harikavonguri@gmail.com>
2025-03-18 15:15:31 +1300

    Initial commit with basic UI code

--- Frontend/Interview APP/src/App.jsx
+++ Frontend/Interview APP/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Mission 3</h1>
+    <div className="container">
+      <h1>AI Mock Interviewer</h1>
+      <div className="job-title">
+        <p>Job Title:</p>
+        <input type="textarea" placeholder="Job Title" className="role" />
       </div>
-    </>
+      <input className="AI-response" />
+      <div className="human-response">
+        <input
+          className="response"
+          type="text"
+          placeholder="type your response"
+        />
+        <button type="submit">Submit</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
